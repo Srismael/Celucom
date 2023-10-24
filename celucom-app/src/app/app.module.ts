4aa1da56233128f64768180d5ba49741f84661fb
ismael <ismaelmtzballin@gmail.com>
2023-10-23 19:00:19 -0600

    se agregaron signup y login

--- celucom-app/src/app/app.module.ts
+++ celucom-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './view/login/login.component';
+import { SignupComponent } from './view/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule
