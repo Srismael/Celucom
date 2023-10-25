31b4d95c1a9e6e2c46d207f62aea63bee55f5eb4
ismael <ismaelmtzballin@gmail.com>
2023-10-25 17:43:49 -0600

    se agrego el ruteo y tambien el login

--- celucom-app/src/app/app.module.ts
+++ celucom-app/src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './view/login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+
+const appRoute:Routes=[
+  {path:'' , component:AppComponent},
+  {path:'login' , component:LoginComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoute),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
