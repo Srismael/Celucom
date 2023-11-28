826a73469f4634e621f67c516ff2373b2335dc69
Gerardo <geraldzepedamtz@gmail.com>
2023-11-27 21:20:10 -0600

    Actualizacion 1.2.3

--- celucom-app/src/app/app-routing.module.ts
+++ celucom-app/src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './view/login/login.component';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './view/home/home.component';
 
-const appRoute:Routes=[
-  
-  {path:'login' , component:LoginComponent}
+const routes:Routes = [
+
+  {path:'view/home/',
+  component:HomeComponent
+  },
+  {path:'view/login/',
+  component:LoginComponent
+  },
 ];
-
 @NgModule({
   declarations: [],
   imports: [
